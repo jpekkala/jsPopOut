37ee72d81e6b419f2316bbf05291b0ba753bd65b
Jukka Pekkala <jukka@jpekkala.com>
2020-12-22 04:26:49 +0200

    Reformat

--- src/engine.js
+++ src/engine.js
@@ -4,162 +4,164 @@ WIN = 1;
 LOSS = -1;
 
 /**
- * Performs two-valued negamax with shallow pruning. The history heuristic is 
+ * Performs two-valued negamax with shallow pruning. The history heuristic is
  * used for move ordering and a transposition table is used to cache results.
  *
  * The game engine calculates whether the first player wins or loses under
- * "handicapping rules". The handicapping rules require the first player to win  * within 21 plies and does not allow him to make a pop move unless it leads to  * an immediate him. In other words, a loss for the first player does not 
- * necessarily mean that he loses in PopOut. It is possible that he could win 
- * if he made a non-winning pop move or if the game was allowed to last longer 
+ * "handicapping rules". The handicapping rules require the first player to win
+ * within 21 plies and does not allow him to make a pop move unless it leads to
+ * an immediate him. In other words, a loss for the first player does not
+ * necessarily mean that he loses in PopOut. It is possible that he could win
+ * if he made a non-winning pop move or if the game was allowed to last longer
  * than 21 plies.
  */
-function GameEngine () {
-	Bitboard.call(this);
-	this.transTable = new TransTable();
-	this.history = new Array(7*6);
+function GameEngine() {
+    Bitboard.call(this);
+    this.transTable = new TransTable();
+    this.history = new Array(7 * 6);
 }
 
 GameEngine.prototype = Object.create(Bitboard.prototype);
 
 /**
- * Sets up everything and performs the search. Returns either WIN or LOSS 
+ * Sets up everything and performs the search. Returns either WIN or LOSS
  * which is from the perspective of the current player.
  */
-GameEngine.prototype.solve = function() {
-	if(this.hasWon(this.boards[0])) {
-		return this.ply % 2 == 0?WIN:LOSS;
-	} else if(this.hasWon(this.boards[1])) {
-		return this.ply % 2 == 0?LOSS:WIN;
-	}
+GameEngine.prototype.solve = function () {
+    if (this.hasWon(this.boards[0])) {
+        return this.ply % 2 == 0 ? WIN : LOSS;
+    } else if (this.hasWon(this.boards[1])) {
+        return this.ply % 2 == 0 ? LOSS : WIN;
+    }
 
-	this.resetHistory();
-	this.interiorCount = 0;	
-	this.plyLimit = 21;
-	return this.negamax();
+    this.resetHistory();
+    this.interiorCount = 0;
+    this.plyLimit = 21;
+    return this.negamax();
 }
 
 /**
  * Returns a winning move or false if no such move could be found
  */
-GameEngine.prototype.getBestMove = function() {
-	var symmetric = this.getPositionCode() == this.getMirrorPositionCode();
-	var succ = this.getSuccessors(symmetric);
-	var best;	
-	//check if there is an immediate win
-	var quickScore = this.fastEvaluate(succ);
-	if(quickScore == WIN) {
-		for(var i = 0; i < succ.length; i++) {
-			if(succ[i].score == WIN) {
-				best = succ[i];
-				break;
-			}	
-		}
-	} else {
-		//no immediate win, call negamax
-		var oldVariation = this.variation;
-		this.resetHistory();
-		this.orderMoves(succ);
-		for(var i = 0; i < succ.length; i++) {
-			var s = succ[i];
-			if(s.score != UNKNOWN) continue;
-			if(s.pop) this.pop(s.column);
-			else this.drop(s.column);
-			
-			var r = this.solve();
-			this.setVariation(oldVariation);
-			if(r == LOSS) {
-				best = s;
-				break;
-			}
-		}
-	}
+GameEngine.prototype.getBestMove = function () {
+    var symmetric = this.getPositionCode() == this.getMirrorPositionCode();
+    var succ = this.getSuccessors(symmetric);
+    var best;
+    //check if there is an immediate win
+    var quickScore = this.fastEvaluate(succ);
+    if (quickScore == WIN) {
+        for (var i = 0; i < succ.length; i++) {
+            if (succ[i].score == WIN) {
+                best = succ[i];
+                break;
+            }
+        }
+    } else {
+        //no immediate win, call negamax
+        var oldVariation = this.variation;
+        this.resetHistory();
+        this.orderMoves(succ);
+        for (var i = 0; i < succ.length; i++) {
+            var s = succ[i];
+            if (s.score != UNKNOWN) continue;
+            if (s.pop) this.pop(s.column);
+            else this.drop(s.column);
 
-	if(best) {
-		//convert to char a-g or A-G
-		return String.fromCharCode((best.pop?65:97) + best.column);
-	} else {
-		return false;
-	}
+            var r = this.solve();
+            this.setVariation(oldVariation);
+            if (r == LOSS) {
+                best = s;
+                break;
+            }
+        }
+    }
+
+    if (best) {
+        //convert to char a-g or A-G
+        return String.fromCharCode((best.pop ? 65 : 97) + best.column);
+    } else {
+        return false;
+    }
 }
 
 /**
- * Returns the successor states i.e. the states reachable by making only one 
- * move. If the flag symmetric is set, the position has been determined to be 
+ * Returns the successor states i.e. the states reachable by making only one
+ * move. If the flag symmetric is set, the position has been determined to be
  * symmetric and moves in the last three columns are skipped.
  */
-GameEngine.prototype.getSuccessors = function(symmetric) {
-	var p = this.ply & 1;
+GameEngine.prototype.getSuccessors = function (symmetric) {
+    var p = this.ply & 1;
 
-	var columnLimit = symmetric ? 4 : 7;
-	var successors = [];
+    var columnLimit = symmetric ? 4 : 7;
+    var successors = [];
 
-	for(var x = 0; x < columnLimit; x++) {
-		if(this.canDrop(x)) {
-			var s = { boards: this.getDropBoards(x), pop: false, column: x, score: UNKNOWN};
-			successors.push(s);	
-		}
-	}
+    for (var x = 0; x < columnLimit; x++) {
+        if (this.canDrop(x)) {
+            var s = {boards: this.getDropBoards(x), pop: false, column: x, score: UNKNOWN};
+            successors.push(s);
+        }
+    }
 
-	//do in a second loop so that pop moves come last
-	for(var x = 0; x < columnLimit; x++) {
-		if(this.canPop(x)) {
-			var s = { boards: this.getPopBoards(x), pop: true, column: x, score: UNKNOWN};
-			successors.push(s);	
-		}
-	}
-	
-	return successors;
+    //do in a second loop so that pop moves come last
+    for (var x = 0; x < columnLimit; x++) {
+        if (this.canPop(x)) {
+            var s = {boards: this.getPopBoards(x), pop: true, column: x, score: UNKNOWN};
+            successors.push(s);
+        }
+    }
+
+    return successors;
 }
 
 /**
- * Determines if any of the successor states are terminal nodes. Returns WIN 
- * if at least one successor state is a win for the current player, otherwise 
+ * Determines if any of the successor states are terminal nodes. Returns WIN
+ * if at least one successor state is a win for the current player, otherwise
  * LOSS.
  */
-GameEngine.prototype.fastEvaluate = function(successors) {
-	var current = this.ply & 1;
-	var other = current^1;
-	for(var i = 0; i < successors.length; i++) {
-		var s = successors[i];
-		if(this.hasWon(s.boards[current])) {
-			s.score = WIN;
-			return WIN;
-		}	else if(s.pop) {
-			if(current == 0 || this.hasWon(s.boards[other])) {
-				s.score = LOSS;
-			}
-		}
-	}
-	return LOSS;
+GameEngine.prototype.fastEvaluate = function (successors) {
+    var current = this.ply & 1;
+    var other = current ^ 1;
+    for (var i = 0; i < successors.length; i++) {
+        var s = successors[i];
+        if (this.hasWon(s.boards[current])) {
+            s.score = WIN;
+            return WIN;
+        } else if (s.pop) {
+            if (current == 0 || this.hasWon(s.boards[other])) {
+                s.score = LOSS;
+            }
+        }
+    }
+    return LOSS;
 }
 
 /**
- * Resets the history heuristic and initializes the move scores by preferring 
+ * Resets the history heuristic and initializes the move scores by preferring
  * the middle columns.
  */
-GameEngine.prototype.resetHistory = function() {
-	//give middle cells a slightly better initial score
-	for(var x = 0; x < 7; x++) {
-		var v = Math.min(x, 6 - x);
-		for(var y = 0; y < 6; y++) {
-			this.history[x*6 + y] = v;
-		}
-	}
+GameEngine.prototype.resetHistory = function () {
+    //give middle cells a slightly better initial score
+    for (var x = 0; x < 7; x++) {
+        var v = Math.min(x, 6 - x);
+        for (var y = 0; y < 6; y++) {
+            this.history[x * 6 + y] = v;
+        }
+    }
 }
 
-GameEngine.prototype.getHistoryScore = function(x) {
-	return this.history[x*6 + this.heights[x]];
+GameEngine.prototype.getHistoryScore = function (x) {
+    return this.history[x * 6 + this.heights[x]];
 }
 
 /**
- * The history score should be increased if a move is "sufficient" i.e. it 
+ * The history score should be increased if a move is "sufficient" i.e. it
  * caused a cutoff.
  */
-GameEngine.prototype.increaseHistoryScore = function(x) {
-	if(this.ply <= 22) {
-		//the weight 2^depth is used
-		this.history[x*6 + this.heights[x]] += Math.pow(2, 22 - this.ply);
-	}
+GameEngine.prototype.increaseHistoryScore = function (x) {
+    if (this.ply <= 22) {
+        //the weight 2^depth is used
+        this.history[x * 6 + this.heights[x]] += Math.pow(2, 22 - this.ply);
+    }
 }
 
 /**
@@ -168,112 +170,112 @@ GameEngine.prototype.increaseHistoryScore = function(x) {
  * are almost always played only as a last resort and therefore their
  * order is unlikely to matter.
  */
-GameEngine.prototype.orderMoves = function(succ) {
-	for(var i = 1; i < succ.length; i++) {
-		//pop moves should come after drop moves and they do not need to be ordered
-		if(succ[i].pop) break;
+GameEngine.prototype.orderMoves = function (succ) {
+    for (var i = 1; i < succ.length; i++) {
+        //pop moves should come after drop moves and they do not need to be ordered
+        if (succ[i].pop) break;
 
-		//insertion sort based on history score
-		var j = i;
-		while(j > 0) {
-			var a = this.getHistoryScore(succ[j].column);
-			var b = this.getHistoryScore(succ[j - 1].column);
+        //insertion sort based on history score
+        var j = i;
+        while (j > 0) {
+            var a = this.getHistoryScore(succ[j].column);
+            var b = this.getHistoryScore(succ[j - 1].column);
 
-			if(a > b) {
-				//swap
-				var t = succ[j];
-				succ[j] = succ[j - 1];
-				succ[j - 1] = t;
-				j--;
-			} else {
-				//the rest are already sorted
-				break;
-			}
-		}
-	}
+            if (a > b) {
+                //swap
+                var t = succ[j];
+                succ[j] = succ[j - 1];
+                succ[j - 1] = t;
+                j--;
+            } else {
+                //the rest are already sorted
+                break;
+            }
+        }
+    }
 }
 
 /**
- * Two-valued negamax. The method calls itself recursively until it finds the 
- * score for the current state. 
- */ 
-GameEngine.prototype.negamax = function() {
-	this.interiorCount++;
+ * Two-valued negamax. The method calls itself recursively until it finds the
+ * score for the current state.
+ */
+GameEngine.prototype.negamax = function () {
+    this.interiorCount++;
 
-	if(this.reportFunction && this.interiorCount % 200000 == 0) {
-		this.reportFunction();
-	}
+    if (this.reportFunction && this.interiorCount % 200000 == 0) {
+        this.reportFunction();
+    }
 
-	//depth limit reached, White loses
-	if(this.ply >= this.plyLimit) {
-		return this.ply % 2 == 0?LOSS:WIN;
-	}
+    //depth limit reached, White loses
+    if (this.ply >= this.plyLimit) {
+        return this.ply % 2 == 0 ? LOSS : WIN;
+    }
 
-	//find the position code and check for symmetry
-	var pos = this.getPositionCode();
-	var mirrorPos = this.getMirrorPositionCode();
-	var symmetrical = false;
-	if(pos === mirrorPos) {
-		pos = pos < mirrorPos ? pos : mirrorPos;
-		symmetrical = true;
-	}
+    //find the position code and check for symmetry
+    var pos = this.getPositionCode();
+    var mirrorPos = this.getMirrorPositionCode();
+    var symmetrical = false;
+    if (pos === mirrorPos) {
+        pos = pos < mirrorPos ? pos : mirrorPos;
+        symmetrical = true;
+    }
 
-	//check if we have already solved the position
-	var transScore = this.transTable.fetch(pos);
-	if(transScore != UNKNOWN) return transScore;
-	
-	//get successor states and check for an immediate win
-	var successors = this.getSuccessors(symmetrical);
-	var bestScore = this.fastEvaluate(successors);
-	if(bestScore == WIN) return WIN;
+    //check if we have already solved the position
+    var transScore = this.transTable.fetch(pos);
+    if (transScore != UNKNOWN) return transScore;
 
-	//order moves with the history heuristic
-	this.orderMoves(successors);
+    //get successor states and check for an immediate win
+    var successors = this.getSuccessors(symmetrical);
+    var bestScore = this.fastEvaluate(successors);
+    if (bestScore == WIN) return WIN;
 
-	//save the current state
-	var startNodes = this.interiorCount;
-	var oldVariation = this.variation;
-	var oldBoards = this.boards;
+    //order moves with the history heuristic
+    this.orderMoves(successors);
 
-	this.ply++;
-	for(var i = 0; i < successors.length; i++) {
-		var s = successors[i];
-		//if the score is not UNKNOWN, it is a terminal state
-		if(s.score != UNKNOWN) continue;
-	
-		//change the state
-		this.boards = s.boards;
-		if(s.pop) {
-			this.heights[s.column]--;
-			this.variation += String.fromCharCode(65 + s.column);
-		} else {
-			this.heights[s.column]++;
-			this.variation += String.fromCharCode(97 + s.column);
-		}
+    //save the current state
+    var startNodes = this.interiorCount;
+    var oldVariation = this.variation;
+    var oldBoards = this.boards;
 
-		//recursive call, note the minus sign
-		var newScore = -this.negamax();
+    this.ply++;
+    for (var i = 0; i < successors.length; i++) {
+        var s = successors[i];
+        //if the score is not UNKNOWN, it is a terminal state
+        if (s.score != UNKNOWN) continue;
 
-		//return to the previous state
-		if(s.pop) {
-			this.heights[s.column]++;
-		} else {
-			this.heights[s.column]--;
-		}
-		this.variation = oldVariation;
+        //change the state
+        this.boards = s.boards;
+        if (s.pop) {
+            this.heights[s.column]--;
+            this.variation += String.fromCharCode(65 + s.column);
+        } else {
+            this.heights[s.column]++;
+            this.variation += String.fromCharCode(97 + s.column);
+        }
 
-		//if we have found a winning move, we can skip the rest of the moves
-		if(newScore == WIN) {
-			this.increaseHistoryScore(s.column);
-			bestScore = WIN;
-			break;
-		}
-	}
-	this.ply--;
+        //recursive call, note the minus sign
+        var newScore = -this.negamax();
 
-	//restore old state completely
-	this.boards = oldBoards;
+        //return to the previous state
+        if (s.pop) {
+            this.heights[s.column]++;
+        } else {
+            this.heights[s.column]--;
+        }
+        this.variation = oldVariation;
 
-	this.transTable.store(pos, bestScore, this.interiorCount - startNodes);
-	return bestScore;
+        //if we have found a winning move, we can skip the rest of the moves
+        if (newScore == WIN) {
+            this.increaseHistoryScore(s.column);
+            bestScore = WIN;
+            break;
+        }
+    }
+    this.ply--;
+
+    //restore old state completely
+    this.boards = oldBoards;
+
+    this.transTable.store(pos, bestScore, this.interiorCount - startNodes);
+    return bestScore;
 }
--- src/transtable.js
+++ src/transtable.js
@@ -7,80 +7,80 @@
  * with the greatest work is always kept together with the new position.
  *
  * The information must be encoded in 52 bits because of Javascript's number
- * type. The score takes only one bit because the search algorithm uses 
- * two-valued logic. The number of bits for the key depends on the 
+ * type. The score takes only one bit because the search algorithm uses
+ * two-valued logic. The number of bits for the key depends on the
  * transposition table size. The remaining bits are reserved for the work.
  */
 function TransTable(size) {
-	//the size should be a prime because it is used as the hash function
-	this.transSize = typeof size !== 'undefined'?size:2097169;
+    //the size should be a prime because it is used as the hash function
+    this.transSize = typeof size !== 'undefined' ? size : 2097169;
 
-	//a position uses 49 bits, calculate how many bits the key needs
-	this.keyBits = 49 - Math.floor(Math.log(this.transSize)/Math.log(2));
-	//calculating the mask fails if key is 31 bits or more
-	if(this.keyBits > 30) throw new Error("Transposition table size is too small");
-	this.scoreOffset = 1 << this.keyBits;
-	this.keyMask = this.scoreOffset - 1;
-	this.maxWork = Math.pow(2, 52 - 1 - this.keyBits) - 1;
+    //a position uses 49 bits, calculate how many bits the key needs
+    this.keyBits = 49 - Math.floor(Math.log(this.transSize) / Math.log(2));
+    //calculating the mask fails if key is 31 bits or more
+    if (this.keyBits > 30) throw new Error("Transposition table size is too small");
+    this.scoreOffset = 1 << this.keyBits;
+    this.keyMask = this.scoreOffset - 1;
+    this.maxWork = Math.pow(2, 52 - 1 - this.keyBits) - 1;
 
-	this.reset();
+    this.reset();
 }
 
-TransTable.prototype.reset = function() {
-	this.table = new Array(this.transSize * 2);
+TransTable.prototype.reset = function () {
+    this.table = new Array(this.transSize * 2);
 }
 
-TransTable.prototype.store = function(pos, score, nodes) {
-	//could also take log_2 or similar to ensure it fits
-	if(nodes > this.maxWork) {
-		nodes = this.maxWork;
-	}
+TransTable.prototype.store = function (pos, score, nodes) {
+    //could also take log_2 or similar to ensure it fits
+    if (nodes > this.maxWork) {
+        nodes = this.maxWork;
+    }
 
-	//there are two entries per transposition slot
-	var index = (pos % this.transSize) * 2;
-	//Javascript does not have integer division
-	var key = Math.floor(pos / this.transSize);
+    //there are two entries per transposition slot
+    var index = (pos % this.transSize) * 2;
+    //Javascript does not have integer division
+    var key = Math.floor(pos / this.transSize);
 
-	//combine score and work
-	var scoreWork = 2 * nodes;
-	if(score === 1) scoreWork += 1;
+    //combine score and work
+    var scoreWork = 2 * nodes;
+    if (score === 1) scoreWork += 1;
 
-	//multiplying by this.scoreOffset is the same as shifting (exceeds 32 bits here)
-	var entry = scoreWork * this.scoreOffset + key;
+    //multiplying by this.scoreOffset is the same as shifting (exceeds 32 bits here)
+    var entry = scoreWork * this.scoreOffset + key;
 
-	//determine if the new position has the biggest work
-	var first = this.table[index];
-	if(!first || entry > first) {
-		//store as the first entry and move the old one to second place
-		this.table[index] = entry;
-		this.table[index + 1] = first;
-	} else {
-		//not the biggest work, store in the second place
-		this.table[index + 1] = entry;
-	}
+    //determine if the new position has the biggest work
+    var first = this.table[index];
+    if (!first || entry > first) {
+        //store as the first entry and move the old one to second place
+        this.table[index] = entry;
+        this.table[index + 1] = first;
+    } else {
+        //not the biggest work, store in the second place
+        this.table[index + 1] = entry;
+    }
 }
 
-TransTable.prototype.fetch = function(pos) {
-	var index = (pos % this.transSize) * 2;
-	var key = Math.floor(pos / this.transSize);
+TransTable.prototype.fetch = function (pos) {
+    var index = (pos % this.transSize) * 2;
+    var key = Math.floor(pos / this.transSize);
 
-	//check the first entry
-	var entry = this.table[index];
-	if(!entry) return 0;
-	if((entry & this.keyMask) === key) {
-		//the score is in the first 32 bits so shifting is safe
-		var whiteWins = (entry >>> this.keyBits & 1) == 1;	
-		return whiteWins?1:-1;
-	}
+    //check the first entry
+    var entry = this.table[index];
+    if (!entry) return 0;
+    if ((entry & this.keyMask) === key) {
+        //the score is in the first 32 bits so shifting is safe
+        var whiteWins = (entry >>> this.keyBits & 1) == 1;
+        return whiteWins ? 1 : -1;
+    }
 
-	//check the second entry
-	entry = this.table[index + 1];
-	if(!entry) return 0;
-	if((entry & this.keyMask) === key) {
-		var whiteWins = (entry >>> this.keyBits & 1) == 1;	
-		return whiteWins?1:-1;
-	}
+    //check the second entry
+    entry = this.table[index + 1];
+    if (!entry) return 0;
+    if ((entry & this.keyMask) === key) {
+        var whiteWins = (entry >>> this.keyBits & 1) == 1;
+        return whiteWins ? 1 : -1;
+    }
 
-	return 0;
+    return 0;
 }
 
